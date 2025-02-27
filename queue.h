55e61d224c15c650a5cf479ef15679ff795e9c8d
Sara <sara.nemati@yaelev.se>
2025-02-27 16:10:11 +0100

    added comments

--- queue.h
+++ queue.h
@@ -1,3 +1,18 @@
+/**
+ * @file queue.h
+ * @author Sara Nemati (sara.nemati@yaelev.se)
+ * @brief header file for the queue class, including the queue class and the IMemory class.
+ *        functions in the queue class: clear, dequeue, enqueue, movable, average, resize (and also size, isFull and capacity).
+ *        functions in the IMemory class: malloc, free.
+ *        the queue class is a circular queue that can be resized at runtime.
+ *        the queue class is movable but not copyable.
+ *
+ * @version 0.1
+ * @date 2025-02-27
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
 #ifndef QUEUE_H
 #define QUEUE_H
 
@@ -7,47 +22,47 @@
 #include <new>
 #include <vector>
 
-struct IMemory
+struct IMemory // initailizing the IMemory class
 {
     virtual void *malloc(size_t size) = 0;
     virtual void free(void *ptr) = 0;
     virtual ~IMemory() = default;
 };
 
-template <typename T>
+template <typename T> // initializing the queue class
 class Queue
 {
 
-    struct node_t
+    struct node_t // initializing the node_t struct
     {
         T data;
         node_t *next{nullptr};
     };
 
-    IMemory &memory;
-    size_t count{0};
-    size_t maxSize; // Now runtime-configurable
-    node_t *head{nullptr};
+    IMemory &memory;       // initializing the memory class
+    size_t count{0};       // runtime-configurable
+    size_t maxSize;        // runtime-configurable
+    node_t *head{nullptr}; // initializing the head and tail
     node_t *tail{nullptr};
 
 public:
-    Queue(const Queue &) = delete;
+    Queue(const Queue &) = delete; // deleting the copy constructor and the copy assignment operator
     Queue &operator=(const Queue &) = delete;
-
+    // defining the constructor
     Queue(IMemory &_memory, size_t size) : memory{_memory}, maxSize{size}
     {
-        if (maxSize < 3)
+        if (maxSize < 3) // checking if the size is less than 3
         {
-            throw std::invalid_argument("Size of queue less than 3.");
+            throw std::invalid_argument("Size of queue less than 3."); // throw an exception for invalid size
         }
 
-        for (size_t i = 0; i < maxSize; i++)
+        for (size_t i = 0; i < maxSize; i++) // initializing the queue with the size
         {
-            node_t *node = static_cast<node_t *>(memory.malloc(sizeof(node_t)));
+            node_t *node = static_cast<node_t *>(memory.malloc(sizeof(node_t))); // allocating memory for the node
 
-            if (node == nullptr)
+            if (node == nullptr) // checking if the memory allocation failed
             {
-                // release memory
+                // release memory for all nodes allocated so far
                 while (head != nullptr)
                 {
                     tail = head;
@@ -56,12 +71,12 @@ public:
                     memory.free(tail);
                 }
 
-                throw std::bad_alloc();
+                throw std::bad_alloc(); // throw an exception for bad allocation
             }
 
-            (void)new (node) node_t;
+            (void)new (node) node_t; // initializing the node
 
-            if (head == nullptr)
+            if (head == nullptr) // checking if the head is null and initializing the head and tail
             {
                 head = node;
                 tail = head;
@@ -73,32 +88,32 @@ public:
             }
         }
 
-        tail->next = head;
+        tail->next = head; // linking the tail to the head creating a circular queue
     }
 
-    Queue(Queue &&that) noexcept;
+    Queue(Queue &&that) noexcept; // moving constructor
 
-    Queue &operator=(Queue &&that) noexcept;
+    Queue &operator=(Queue &&that) noexcept; // moving assignment operator
 
-    void enqueue(const T &item);
+    void enqueue(const T &item); // initializing the enqueue function
 
-    bool dequeue(T &item);
+    bool dequeue(T &item); // initializing the dequeue function
 
-    bool isFull();
+    bool isFull(); // initializing the isFull function
 
-    double average();
+    double average(); // initializing the average function
 
-    void resize(size_t num);
+    void resize(size_t num); // initializing the resize function
 
-    size_t size(void) { return count; }
+    size_t size(void) { return count; } // defining the size function
 
-    size_t capacity(void) { return maxSize; }
+    size_t capacity(void) { return maxSize; } // defining the capacity function
 
-    void clear(void);
+    void clear(void); // initializing the clear function
 
-    ~Queue()
+    ~Queue() // defining the destructor
     {
-        for (size_t i = 0; i < maxSize; i++)
+        for (size_t i = 0; i < maxSize; i++) // removing and releasing the memory for all nodes
         {
             tail = head;
             head = head->next;
@@ -108,7 +123,14 @@ public:
     }
 };
 
-template <typename T>
+/**
+ * @brief moving constructor
+ * 
+ *
+ * @tparam T 
+ * @param that 
+ */
+template <typename T> 
 Queue<T>::Queue(Queue<T> &&that) noexcept : memory{that.memory}, count{that.count}, maxSize{that.maxSize}, head{that.head}, tail{that.tail}
 {
     that.count = 0;
@@ -117,7 +139,15 @@ Queue<T>::Queue(Queue<T> &&that) noexcept : memory{that.memory}, count{that.coun
     that.tail = nullptr;
 }
 
-template <typename T>
+/**
+ * @brief moving assignment operator
+ * 
+ *
+ * @tparam T 
+ * @param that 
+ * @return *this
+ */
+template <typename T> 
 Queue<T> &Queue<T>::operator=(Queue<T> &&that) noexcept
 {
     if (this != &that)
@@ -139,22 +169,37 @@ Queue<T> &Queue<T>::operator=(Queue<T> &&that) noexcept
     return *this;
 }
 
+/**
+ * @brief enqueue function to write an item to the queue and overwrite the oldest item if the queue is full
+ *
+ * @tparam T
+ * @param item
+ */
 template <typename T>
 void Queue<T>::enqueue(const T &item)
 {
-    tail = tail->next;
-    tail->data = item;
+    tail = tail->next; // moving the tail to the next node
+    tail->data = item; // adding the item to the tail
 
-    if (!isFull())
+    if (!isFull()) // if queue is not full increment the count
     {
         count++;
     }
-    else
+    else // if queue is full overwrite and move the head to the next node
     {
         head = head->next;
     }
 }
 
+/**
+ * @brief dequeue function to read an item from the queue
+ *
+ *
+ * @tparam T
+ * @param item
+ * @return true
+ * @return false
+ */
 template <typename T>
 bool Queue<T>::dequeue(T &item)
 {
@@ -162,59 +207,80 @@ bool Queue<T>::dequeue(T &item)
 
     if (count > 0)
     {
-        item = head->data;
-        head = head->next;
-        count--;
-        status = true;
+        item = head->data; // reading the item from the head
+        head = head->next; // moving the head to the next node
+        count--;           // decrementing the count
+        status = true;     // setting the status to true
     }
 
     return status;
 }
 
+/**
+ * @brief is full function to check if the queue is full
+ *
+ * @tparam T
+ * @return true
+ * @return false
+ */
 template <typename T>
 bool Queue<T>::isFull()
 {
-    return count == maxSize;
+    return count == maxSize; // checking if the count is equal to the capacity of the queue
 }
 
+/**
+ * @brief average function to calculate the average of the elements in the queue if the type is arithmetic
+ *
+ *
+ * @tparam T
+ * @return double
+ */
 template <typename T>
 double Queue<T>::average()
 {
-    double value{0};
-    static_assert(std::is_arithmetic<T>::value, "average() only works for arithmetic types");
+    double value{0};                                                                          // initializing the value
+    static_assert(std::is_arithmetic<T>::value, "average() only works for arithmetic types"); // checking if the type is arithmetic
 
-    if (count > 0)
+    if (count > 0) // if the count is greater than 0 calculate the average
     {
-        node_t *current = head;
-        for (size_t i = 0; i < count; i++)
+        node_t *current = head;            // initializing the current node
+        for (size_t i = 0; i < count; i++) // calculating the average
         {
-            value += static_cast<double>(current->data);
-            current = current->next;
+            value += static_cast<double>(current->data); // adding the value of the current node to the value
+            current = current->next;                     // moving to the next node
         }
 
-        value = value / count;
+        value = value / count; // calculating the average
     }
 
-    return value;
+    return value; // returning the average
 }
 
+/**
+ * @brief resize function to resize the queue at runtime, increase or decrease size, adding or removing nodes accordingly
+ *
+ *
+ * @tparam T
+ * @param num
+ */
 template <typename T>
 void Queue<T>::resize(size_t num)
 {
-    if (num < 3)
+    if (num < 3) // checking if the size is less than 3
     {
-        throw std::invalid_argument("size too small.");
+        throw std::invalid_argument("size too small."); // throw an exception for invalid size
     }
 
-    if (num > maxSize)
+    if (num > maxSize) // for increasing the size
     {
-        for (size_t i = maxSize; i < num; i++)
+        for (size_t i = maxSize; i < num; i++) // adding nodes to the queue starting from current size to the new size
         {
-            node_t *node = reinterpret_cast<node_t *>(memory.malloc(sizeof(node_t)));
+            node_t *node = reinterpret_cast<node_t *>(memory.malloc(sizeof(node_t))); // allocating memory for the node
 
-            if (node == nullptr)
+            if (node == nullptr) // checking if the memory allocation failed
             {
-                for (size_t j = maxSize; j < i; j++)
+                for (size_t j = maxSize; j < i; j++) // releasing memory for all nodes allocated so far
                 {
                     node = tail->next;
                     tail->next = node->next;
@@ -222,37 +288,44 @@ void Queue<T>::resize(size_t num)
                     memory.free(node);
                 }
 
-                throw std::bad_alloc();
+                throw std::bad_alloc(); // throw an exception for bad allocation
             }
-            else
+            else // if memory allocation is successful
             {
-                (void)new (node) node_t;
-                node->next = tail->next;
+                (void)new (node) node_t; // initializing the node
+                node->next = tail->next; // inserting the node between tail->next and tail
                 tail->next = node;
             }
         }
     }
-    else if (num < maxSize)
+    else if (num < maxSize) // for decreasing the size
     {
-        node_t *node{nullptr};
-        for (size_t i = num; i < maxSize; i++)
+        node_t *node{nullptr};                 // initializing the node used to delete nodes
+        for (size_t i = num; i < maxSize; i++) // removing nodes from the queue starting from new size to current size
         {
-            node = tail->next;
-            if (node == head)
+            node = tail->next; // deleting the node after the tail
+            if (node == head)  // if the node is the head move the head to the next node
             {
                 head = head->next;
-                count--;
+                count--; // decrementing the count
             }
-            tail->next = node->next;
+            tail->next = node->next; // setting the tail->next to the next node after the node that shall be deleted
 
             node->~node_t();
             memory.free(node);
         }
     }
 
-    maxSize = num;
+    maxSize = num; // setting the new size accordingly
 }
 
+/**
+ * @brief clear function to reset the queue to its initial state, by resetting the count and moving the head to the next node
+ *
+ *
+ * @tparam T
+ * @param
+ */
 template <typename T>
 void Queue<T>::clear(void)
 {
--- test.cpp
+++ test.cpp
@@ -1,3 +1,14 @@
+/**
+ * @file test.cpp
+ * @author Sara Nemati (sara.nemati@yaelev.se)
+ * @brief test file for the queue class, including tests for the functions in the queue class: 
+ *                  clear, dequeue, enqueue, movable, average, resize.
+ * @version 0.1
+ * @date 2025-02-27
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
 #include "queue.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
@@ -6,18 +17,18 @@ using ::testing::_;
 using ::testing::Invoke;
 using ::testing::NiceMock;
 using ::testing::Return;
-
+// initializing the memory class
 class Memory : public IMemory
 {
     size_t pos{0};
-    uint8_t array[8192]; // Increase size of array if required
+    uint8_t array[8192];
     std::vector<void *> vec;
 
 public:
-    MOCK_METHOD(void *, malloc, (size_t size), (override));
-    MOCK_METHOD(void, free, (void *ptr), (override));
+    MOCK_METHOD(void *, malloc, (size_t size), (override)); // mocking the malloc function
+    MOCK_METHOD(void, free, (void *ptr), (override));       // mocking the free function
 
-    void *allocate(size_t size)
+    void *allocate(size_t size) // allocating memory
     {
         void *ptr{nullptr};
 
@@ -31,17 +42,17 @@ public:
         return ptr;
     }
 
-    void release(void *ptr)
+    void release(void *ptr) // releasing memory
     {
         vec.erase(std::remove(vec.begin(), vec.end(), ptr), vec.end());
     }
 
-    ~Memory() { EXPECT_EQ(0, vec.size()); }
+    ~Memory() { EXPECT_EQ(0, vec.size()); } // destructor
 };
-
+// initializing the queue class for testing
 template <typename T>
 class QueueFixture : public ::testing::Test
-{
+{ // initializing the values
     const std::tuple<
         std::vector<int>,
         std::vector<float>,
@@ -52,82 +63,82 @@ class QueueFixture : public ::testing::Test
             {"aaa", "bbb", "ccc", "ddd", "eee"}};
 
 protected:
-    const std::vector<T> values{std::get<std::vector<T>>(allValues)};
-    NiceMock<Memory> mock;
-    Queue<T> *queue{nullptr};
+    const std::vector<T> values{std::get<std::vector<T>>(allValues)}; // getting the values
+    NiceMock<Memory> mock;                                            // mocking the memory class
+    Queue<T> *queue{nullptr};                                         // initializing the queue
 
-    void SetUp(void) override
+    void SetUp(void) override // setting up the queue
     {
-        ON_CALL(mock, malloc(_))
+        ON_CALL(mock, malloc(_)) // mocking the malloc function
             .WillByDefault(Invoke(&mock, &Memory::allocate));
 
-        ON_CALL(mock, free(_))
+        ON_CALL(mock, free(_)) // mocking the free function
             .WillByDefault(Invoke(&mock, &Memory::release));
 
-        queue = new Queue<T>{mock, values.size()};
+        queue = new Queue<T>{mock, values.size()}; // creating the queue
 
-        for (size_t i = 1; i <= values.size(); i++)
+        for (size_t i = 1; i <= values.size(); i++) // enqueuing the values
         {
             queue->enqueue(values[i - 1]);
             EXPECT_EQ(i, queue->size());
         }
     }
 
-    void TearDown(void) override
+    void TearDown(void) override // tearing down the queue
     {
         delete queue;
     }
 };
-
+// initializing the test types
 using TestTypes = ::testing::Types<int, float, std::string>;
-TYPED_TEST_SUITE(QueueFixture, TestTypes);
+TYPED_TEST_SUITE(QueueFixture, TestTypes); // creating the test suite
 
-TYPED_TEST(QueueFixture, InvalidSize)
+TYPED_TEST(QueueFixture, InvalidSize) // testing the invalid size
 {
     EXPECT_THROW(Queue<TypeParam>(this->mock, 2), std::invalid_argument);
 }
 
-TYPED_TEST(QueueFixture, mallocFails)
+TYPED_TEST(QueueFixture, mallocFails) // testing the malloc function
 {
-    EXPECT_CALL(this->mock, malloc(_))
+    EXPECT_CALL(this->mock, malloc(_)) // calling the malloc function
         .WillOnce(Return(nullptr));
-    EXPECT_THROW(Queue<TypeParam>(this->mock, this->values.size()), std::bad_alloc);
+    EXPECT_THROW(Queue<TypeParam>(this->mock, this->values.size()), std::bad_alloc); // throw expected for bad allocation
 
-    EXPECT_CALL(this->mock, malloc(_))
-        .WillOnce(Invoke(&this->mock, &Memory::allocate))
-        .WillOnce(Invoke(&this->mock, &Memory::allocate))
-        .WillOnce(Return(nullptr));
-    EXPECT_THROW(Queue<TypeParam>(this->mock, this->values.size()), std::bad_alloc);
+    EXPECT_CALL(this->mock, malloc(_))                                               // calling the malloc function
+        .WillOnce(Invoke(&this->mock, &Memory::allocate))                            // allocation successful
+        .WillOnce(Invoke(&this->mock, &Memory::allocate))                            // allocation successful
+        .WillOnce(Return(nullptr));                                                  // allocation failed
+    EXPECT_THROW(Queue<TypeParam>(this->mock, this->values.size()), std::bad_alloc); // throw expected for bad allocation
 }
 
-TYPED_TEST(QueueFixture, testClear)
+TYPED_TEST(QueueFixture, testClear) // testing the clear function
 {
-    TypeParam item;
-    EXPECT_EQ(this->values.size(), this->queue->size());
-    this->queue->clear();
-    EXPECT_EQ(0, this->queue->size());
-    EXPECT_FALSE(this->queue->dequeue(item));
+    TypeParam item;                                      // initializing the item
+    EXPECT_EQ(this->values.size(), this->queue->size()); // checking the size
+    this->queue->clear();                                // calling the clear function to clear the queue
+    EXPECT_EQ(0, this->queue->size());                   // checking the size
+    EXPECT_FALSE(this->queue->dequeue(item));            // expecting false when dequeuing since there is no elements
 }
 
-TYPED_TEST(QueueFixture, testDequeue)
+TYPED_TEST(QueueFixture, testDequeue) // testing the dequeue function
 {
-    TypeParam item;
-    size_t size{this->queue->size()};
+    TypeParam item;                   // initializing the item
+    size_t size{this->queue->size()}; // getting the size
 
-    for (size_t i = 1; i <= size; i++)
+    for (size_t i = 1; i <= size; i++) // dequeuing the elements
     {
         EXPECT_TRUE(this->queue->dequeue(item));
         EXPECT_EQ(size - i, this->queue->size());
         EXPECT_EQ(item, this->values[i - 1]);
     }
 
-    EXPECT_FALSE(this->queue->dequeue(item));
+    EXPECT_FALSE(this->queue->dequeue(item)); // expecting false when dequeuing since there is no elements left
 }
 
-TYPED_TEST(QueueFixture, testEnqueueFull)
+TYPED_TEST(QueueFixture, testEnqueueFull) // testing the enqueue function when the queue is full
 {
-    TypeParam item;
-    EXPECT_TRUE(this->queue->isFull());
+    TypeParam item;                     // initializing the item
+    EXPECT_TRUE(this->queue->isFull()); // expecting true since the queue is full at initialization
 
     // Overwrite oldest element
     this->queue->enqueue(this->values[0]);
@@ -139,60 +150,60 @@ TYPED_TEST(QueueFixture, testEnqueueFull)
     EXPECT_EQ(item, this->values[1]);
 }
 
-TYPED_TEST(QueueFixture, testOnlyMovable)
+TYPED_TEST(QueueFixture, testOnlyMovable) // testing if the queue is movable
 {
-    EXPECT_FALSE(std::is_copy_constructible<Queue<TypeParam>>::value);
-    EXPECT_FALSE(std::is_copy_assignable<Queue<TypeParam>>::value);
+    EXPECT_FALSE(std::is_copy_constructible<Queue<TypeParam>>::value); // checking if the queue is copy constructible
+    EXPECT_FALSE(std::is_copy_assignable<Queue<TypeParam>>::value);    // checking if the queue is copy assignable
 
-    EXPECT_TRUE(std::is_move_constructible<Queue<TypeParam>>::value);
-    EXPECT_TRUE(std::is_move_assignable<Queue<TypeParam>>::value);
+    EXPECT_TRUE(std::is_move_constructible<Queue<TypeParam>>::value);  // checking if the queue is move constructible
+    EXPECT_TRUE(std::is_move_assignable<Queue<TypeParam>>::value);     // checking if the queue is move assignable
 
-    Queue<TypeParam> temp{std::move(*this->queue)};
-    EXPECT_EQ(this->values.size(), temp.size());
-    EXPECT_EQ(0, this->queue->size());
+    Queue<TypeParam> temp{std::move(*this->queue)};                    // moving the queue to a temporary queue
+    EXPECT_EQ(this->values.size(), temp.size());                       // the size of the temporary queue should be equal to the values
+    EXPECT_EQ(0, this->queue->size());                                 // the size of the original queue should be 0
 
-    *this->queue = std::move(temp);
-    EXPECT_EQ(this->values.size(), this->queue->size());
-    EXPECT_EQ(0, temp.size());
+    *this->queue = std::move(temp);                                    // moving the temporary queue back to the original queue
+    EXPECT_EQ(this->values.size(), this->queue->size());               // the size of the original queue should be equal to the values
+    EXPECT_EQ(0, temp.size());                                         // the size of the temporary queue should be 0
 }
 
-TYPED_TEST(QueueFixture, testAverage)
-{
+TYPED_TEST(QueueFixture, testAverage) // testing the average function
+{                                     // checking if the type is arithmetic
     if constexpr (std::is_arithmetic<TypeParam>::value)
     {
-        double sum = 0;
-        for (const auto &val : this->values)
+        double sum = 0;                      // initializing the sum
+        for (const auto &val : this->values) // calculating the sum
         {
             sum += val;
         }
 
-        double expectedAvg = sum / this->values.size();
-        EXPECT_DOUBLE_EQ(this->queue->average(), expectedAvg);
+        double expectedAvg = sum / this->values.size();        // calculating the expected average
+        EXPECT_DOUBLE_EQ(this->queue->average(), expectedAvg); // checking if the average is equal to the expected average
     }
 }
 
-TYPED_TEST(QueueFixture, testResize)
-{
+TYPED_TEST(QueueFixture, testResize) // testing the resize function
+{                                    // getting the size and testing the resize function with an invalid size
     size_t size = this->values.size();
     EXPECT_THROW(this->queue->resize(2), std::invalid_argument);
 
-    size += 4;
+    size += 4; // increaing variable size and resizing the queue
     EXPECT_NO_THROW(this->queue->resize(size));
     EXPECT_FALSE(this->queue->isFull());
     EXPECT_EQ(this->values.size(), this->queue->size());
-    EXPECT_EQ(size, this->queue->capacity());
+    EXPECT_EQ(size, this->queue->capacity()); // size should be equal to the capacity
 
-    EXPECT_CALL(this->mock, malloc(_))
-        .WillOnce(Return(nullptr));
-    EXPECT_THROW(this->queue->resize(size + 3), std::bad_alloc);
-    EXPECT_EQ(this->values.size(), this->queue->size());
-    EXPECT_EQ(size, this->queue->capacity());
+    EXPECT_CALL(this->mock, malloc(_))                           // calling the malloc function
+        .WillOnce(Return(nullptr));                              // allocation failed
+    EXPECT_THROW(this->queue->resize(size + 3), std::bad_alloc); // throw expected for bad allocation
+    EXPECT_EQ(this->values.size(), this->queue->size());         // size of the queue should be equal to the values
+    EXPECT_EQ(size, this->queue->capacity());                    // the capacity should be equal to variable size
 
-    EXPECT_CALL(this->mock, malloc(_))
-        .WillOnce(Invoke(&this->mock, &Memory::allocate))
-        .WillOnce(Invoke(&this->mock, &Memory::allocate))
-        .WillOnce(Return(nullptr));
-    EXPECT_THROW(this->queue->resize(size + 4), std::bad_alloc);
-    EXPECT_EQ(this->values.size(), this->queue->size());
-    EXPECT_EQ(size, this->queue->capacity());
+    EXPECT_CALL(this->mock, malloc(_))                           // calling the malloc function
+        .WillOnce(Invoke(&this->mock, &Memory::allocate))        // allocation successful
+        .WillOnce(Invoke(&this->mock, &Memory::allocate))        // allocation successful
+        .WillOnce(Return(nullptr));                              // allocation failed
+    EXPECT_THROW(this->queue->resize(size + 4), std::bad_alloc); // throw expected for bad allocation
+    EXPECT_EQ(this->values.size(), this->queue->size());         // size of the queue should be equal to the values
+    EXPECT_EQ(size, this->queue->capacity());                    // the capacity should be equal to variable size
 }
